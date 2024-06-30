d8f3b8b232be7ea3fc2ffbb4bb459e0a3b47fc87
Grewo <info@grew-development.de>
2024-06-30 16:17:46 +0200

    [Update] EmbedsManager converted  to Singolton

--- src/main/java/de/grewdev/EmbedsManager.java
+++ src/main/java/de/grewdev/EmbedsManager.java
@@ -1,31 +1,47 @@
 package de.grewdev;
 
-import de.grewdev.embeds.LinksEmbed;
-import de.grewdev.embeds.RuleEmbed;
-import de.grewdev.embeds.products.GrewVehicleshop;
 import de.grewdev.utils.DatabaseConnection;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
-import org.jooq.DSLContext;
-import org.jooq.Record2;
-import org.jooq.SQLDialect;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jooq.*;
+import org.jooq.Record;
+import org.jooq.exception.DataTypeException;
+import org.jooq.exception.MappingException;
+import org.jooq.impl.DSL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
-import static org.jooq.impl.DSL.*;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static org.jooq.impl.DSL.field;
+import static org.jooq.impl.DSL.table;
 import static org.jooq.impl.SQLDataType.VARCHAR;
 
 public class EmbedsManager {
 
     private static final Logger logger = LoggerFactory.getLogger(EmbedsManager.class);
 
-    private final DSLContext dBase;
-    private final JDA jda;
+    private JDA jda;
+    private DSLContext dBase;
+    private static EmbedsManager instance;
 
-    private void createIfNotExistsEmbedTable(String tableName) {
+    public static EmbedsManager getInstance(JDA jda) {
+        if (instance == null) {
+            instance = new EmbedsManager(jda);
+        }
+        return instance;
+    }
+
+    private void createIfNotExistsEmbedTable(String tableName){
         dBase.createTableIfNotExists(tableName)
                 .column("embedName", VARCHAR(36))
                 .column("msgId", VARCHAR(25))
@@ -34,14 +50,7 @@ public class EmbedsManager {
                 .execute();
     }
 
-    private Record2<String, String> getMessageIdIfExists(String embedName, String tableName) {
-        return dBase.select(field("embedName", String.class), field("msgId", String.class))
-                .from(table(tableName))
-                .where(field("embedName", String.class).eq(embedName))
-                .fetchOne();
-    }
-
-    private void setMessageId(String embedName, String tableName, String msgId, String channelId) {
+    private void setMessageId(String tableName, String embedName, String msgId, String channelId) {
         dBase.insertInto(table(tableName))
                 .columns(field("embedName"), field("msgId"), field("channelId"))
                 .values(embedName, msgId, channelId)
@@ -49,37 +58,12 @@ public class EmbedsManager {
                 .set(field("msgId"),msgId)
                 .set(field("channelId"),channelId)
                 .execute();
+
     }
 
-    private void createOrUpdateEmbed(String embedName, String tableName, String channelId, MessageEmbed embed) {
-        Record2<String, String> record = getMessageIdIfExists(embedName, tableName);
-
-        TextChannel chan = jda.getTextChannelById(channelId);
-
-        if (chan == null) {
-            logger.warn("Could not find channel <#{}> for embed <{}>", channelId, embedName);
-            return;
-        }
-
-        try {
-            if (record != null) {
-                String msgId = (String) record.getValue("msgId");
-                Message msg = chan.retrieveMessageById(msgId).submit().exceptionally((ex) -> null).get();
-                if (msg != null) {
-                    msg.editMessageEmbeds(embed).queue();
-                    return;
-                }
-            }
-            Message msg = chan.sendMessageEmbeds(embed).complete();
-            setMessageId(embedName, tableName, msg.getId(), chan.getId());
-        } catch (Exception e) {
-            logger.warn("Error while creating or updating embed <{}> in channel <#{}>",embedName,channelId,e);
-        }
-    }
-
-    public EmbedsManager(JDA jda) {
+    private EmbedsManager(JDA jda) {
         Connection connection = DatabaseConnection.getInstance().getConnection();
-        this.dBase = using(connection, SQLDialect.MYSQL);
+        this.dBase = DSL.using(connection, SQLDialect.MYSQL);
         this.jda = jda;
 
         //create EmbedsTables
@@ -89,12 +73,44 @@ public class EmbedsManager {
         createIfNotExistsEmbedTable("otherEmbeds");
     }
 
-    public void createOrUpdateEmbeds() {
-        //Fix Embed
-        createOrUpdateEmbed("ruleEmbed","fixedEmbeds", System.getenv("CHAN_RULES"),new RuleEmbed());
-        createOrUpdateEmbed("linkEmbed","fixedEmbeds", System.getenv("CHAN_LINKS"),new LinksEmbed());
+    private String getMessageIdIfExist(String embedName, String tableName) {
+        Record1<String> result = dBase.select(field("msgId", String.class))
+                .from(table(tableName))
+                .where(field("embedName", String.class).eq(embedName))
+                .fetchOne();
 
-        //Products Embed
-        createOrUpdateEmbed("grewVehicleshop","productEmbeds", System.getenv("GREW_VEHICLESHOP"),new GrewVehicleshop());
+        if (result!=null) {
+            return result.getValue("msgId").toString();
+        }
+        return null;
+    }
+
+    public Message createOrUpdateEmbed(String embedName, String tableName, String channelId, MessageEmbed embed) {
+        String msgId = getMessageIdIfExist(embedName, tableName);
+        TextChannel chan = jda.getTextChannelById(channelId);
+
+        if (chan == null) {
+            logger.warn("Could not find channel <#{}> for embed <{}>", channelId, embedName);
+            return null;
+        }
+
+        try {
+            if (msgId != null) {
+                Message msg = chan.retrieveMessageById(msgId).submit().exceptionally(ex -> null).get();
+
+                if (msg != null) {
+                    msg.editMessageEmbeds(embed).complete();
+                    return msg;
+                }
+            }
+        }
+        catch (Exception e) {
+            logger.warn("Error while creating or updating embed <{}> in channel <#{}>",embedName,channelId,e);
+            return null;
+        }
+
+        Message msg = chan.sendMessageEmbeds(embed).complete();
+        setMessageId(tableName, embedName, msg.getId(), chan.getId());
+        return msg;
     }
 }
--- src/main/java/de/grewdev/events/ReadyEventListener.java
+++ src/main/java/de/grewdev/events/ReadyEventListener.java
@@ -1,7 +1,11 @@
 package de.grewdev.events;
 
 import de.grewdev.EmbedsManager;
+import de.grewdev.embeds.LinksEmbed;
+import de.grewdev.embeds.RuleEmbed;
+import de.grewdev.embeds.products.GrewVehicleshop;
 import de.grewdev.utils.TimeStamper;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
@@ -19,7 +23,17 @@ public class ReadyEventListener extends ListenerAdapter {
                     .queue();
         }
 
-        EmbedsManager embedsManager = new EmbedsManager(event.getJDA());
-        embedsManager.createOrUpdateEmbeds();
+        createOrUpdateDefaultEmbeds(event.getJDA());
+    }
+
+    public void createOrUpdateDefaultEmbeds(JDA jda) {
+        EmbedsManager embedsManager = EmbedsManager.getInstance(jda);
+
+        //Fix Embed
+        embedsManager.createOrUpdateEmbed("ruleEmbed","fixedEmbeds", System.getenv("CHAN_RULES"),new RuleEmbed());
+        embedsManager.createOrUpdateEmbed("linkEmbed","fixedEmbeds", System.getenv("CHAN_LINKS"),new LinksEmbed());
+
+        //Products Embed
+        embedsManager.createOrUpdateEmbed("grewVehicleshop","productEmbeds", System.getenv("GREW_VEHICLESHOP"),new GrewVehicleshop());
     }
 }
