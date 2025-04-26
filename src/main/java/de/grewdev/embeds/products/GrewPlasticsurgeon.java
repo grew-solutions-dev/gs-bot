0a71e85938887ff9ace3b34aaf387554c5861665
Grewo <info@grew-development.de>
2025-04-26 14:58:49 +0200

    [Update] Grew Plusticsurgeon to v3.0.0

--- src/main/java/de/grewdev/embeds/products/GrewPlasticsurgeon.java
+++ src/main/java/de/grewdev/embeds/products/GrewPlasticsurgeon.java
@@ -15,6 +15,7 @@ public class GrewPlasticsurgeon extends MessageEmbed {
                         The "Grew Plasticsurgeon" script introduces a plastic surgeon to your game, allowing players to customize their outfits and facial features.
                         All changes are saved permanently, adding a new layer of individuality and roleplay possibilities.
                         With "Grew Plasticsurgeon," you can enhance your gameplay experience and create a more dynamic environment for all players!
+                        This script is compatible with QB-Core and ESX frameworks, giving you the flexibility to integrate it seamlessly into your server setup.
                 """,
                 null,
                 null,
@@ -30,7 +31,7 @@ public class GrewPlasticsurgeon extends MessageEmbed {
                                 System.getenv("LINK_WIKI_URL") + "/grew_plasticsurgeon/required",
                                 false),
                         new Field("Price", "5,00€ (+VAT)", true),
-                        new Field("Current Version", "v2.2.0", true),
+                        new Field("Current Version", "v3.0.0", true),
                         new Field("WIKI", System.getenv("LINK_WIKI_URL") + "/grew_plasticsurgeon/readme", false),
                         new Field("Shop-Page", System.getenv("LINK_STORE_URL") + "/package/5571869", false),
                         new Field("Trailer", "coming-soon", false)
